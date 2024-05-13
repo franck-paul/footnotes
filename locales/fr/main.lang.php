6756c3c458532b7d42ded5ae409e83fc40489dac
franck <carnet.franck.paul@gmail.com>
2024-05-13 10:06:23 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,8 +10,8 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Footnotes'] = '';
-L10n::$locales['footnotes'] = 'Autonomisatateur de note';
-L10n::$locales['Enable Footnotes'] = 'Activer Bigfoot';
+L10n::$locales['Footnotes'] = 'Notes de bas de page à la volée';
+L10n::$locales['footnotes'] = 'footnotes';
+L10n::$locales['Enable Footnotes'] = 'Activer les notes de bas de page à la volée';
 L10n::$locales['Options'] = 'Options';
 L10n::$locales['Activate only in single entry context'] = 'Activer uniquement dans les contextes d\'entrée unique (billet ou page)';
