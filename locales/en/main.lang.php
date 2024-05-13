02e8fc6a3561f71f4b10c95f697c36454e3d5646
franck <carnet.franck.paul@gmail.com>
2024-05-13 09:58:22 +0200

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -3,7 +3,6 @@
  * @package Dotclear
  *
  * @copyright Olivier Meunier & Association Dotclear
- * @copyright GPL-2.0-only
  */
 #
 # DOT NOT MODIFY THIS FILE !
@@ -11,14 +10,8 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Empowering footnotes'] = '';
-L10n::$locales['bigfoot'] = '';
-L10n::$locales['Default'] = '';
-L10n::$locales['Bottom'] = '';
-L10n::$locales['Numeric'] = '';
-L10n::$locales['Enable Bigfoot'] = '';
+L10n::$locales['Footnotes'] = '';
+L10n::$locales['footnotes'] = '';
+L10n::$locales['Enable Footnotes'] = '';
 L10n::$locales['Options'] = '';
-L10n::$locales['Style:'] = '';
-L10n::$locales['Activate on hover'] = '';
 L10n::$locales['Activate only in single entry context'] = '';
-L10n::$locales['Bigfoot'] = '';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -3,7 +3,6 @@
  * @package Dotclear
  *
  * @copyright Olivier Meunier & Association Dotclear
- * @copyright GPL-2.0-only
  */
 #
 # DOT NOT MODIFY THIS FILE !
@@ -11,14 +10,8 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Empowering footnotes'] = 'Autonomisatateur de note';
-L10n::$locales['bigfoot'] = 'bigfoot';
-L10n::$locales['Default'] = 'Défaut';
-L10n::$locales['Bottom'] = 'Pied de page';
-L10n::$locales['Numeric'] = 'Numérique';
-L10n::$locales['Enable Bigfoot'] = 'Activer Bigfoot';
+L10n::$locales['Footnotes'] = '';
+L10n::$locales['footnotes'] = 'Autonomisatateur de note';
+L10n::$locales['Enable Footnotes'] = 'Activer Bigfoot';
 L10n::$locales['Options'] = 'Options';
-L10n::$locales['Style:'] = 'Style :';
-L10n::$locales['Activate on hover'] = 'Activer au survol';
 L10n::$locales['Activate only in single entry context'] = 'Activer uniquement dans les contextes d\'entrée unique (billet ou page)';
-L10n::$locales['Bigfoot'] = 'Bigfoot';
