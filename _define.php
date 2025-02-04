4d6e247a7158e52ea92cc42504255a1dbdf02130
franck <carnet.franck.paul@gmail.com>
2025-02-04 12:04:05 +0100

    Add release date information in _define.php, dcstore.xml and cope with it in github action

--- _define.php
+++ _define.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief footnotes, a plugin for Dotclear 2
  *
@@ -16,6 +17,7 @@ $this->registerModule(
     'Franck Paul',
     '1.0',
     [
+        'date'        => '2003-08-13T13:42:00+0100',
         'requires'    => [['core', '2.30']],
         'permissions' => 'My',
         'type'        => 'plugin',
