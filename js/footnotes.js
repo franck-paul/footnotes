8a277f5836d3cc7f0ca74c037551231e82993f96
franck <carnet.franck.paul@gmail.com>
2026-01-07 13:00:08 +0100

    Code review (CSS/Js)

--- js/footnotes.js
+++ js/footnotes.js
@@ -1,74 +1,8 @@
 /*global dotclear */
 'use strict';
 
-// https://blog.mondediplo.net/la-fin-de-l-innocence#nh1
-
-// Modèle notes
-//
-// Markdown :
-//
-// NNNNNNNNNN = timestamp du billet
-// n = numéro de la note (1 à n) pour le billet
-//
-// Appel de note :
-// <sup id="fnref:tsNNNNNNNNNN.n">
-//   <a href="#fn:tsNNNNNNNNNN.n">n</a>
-// </sup>
-//
-// Note :
-// <li id="#fn:tsNNNNNNNNNN.n">
-//   ... <a href="#fnref:tsNNNNNNNNNN.n">↩︎</a>
-// </li>
-//
-// Il va falloir masquer le lien de retour dans la note affichée à la volée, CSS : a:[href qui débute par #fnref:ts]
-//
-// À noter :
-// - notes dans l'extrait et notes dans le corps collisionnent (même ids)
-//
-// Wiki :
-//
-// n = numéro de la note (1 à n) pour le billet
-//
-// Appel de note :
-// <sup>
-//   [<a href="#wiki-footnote-n" id="rev-wiki-footnote-n">n</a>]
-// </sup>
-// Note :
-// <p>
-//   [<a href="#rev-wiki-footnote-n" id="wiki-footnote-n"></a>] ...
-// </p>
-//
-// Il va falloir masquer le lien de retour dans la note affichée à la volée, CSS : a:[href qui débute par #rev-wiki-footnote-]
-//
-// À noter :
-// - ne fonctionne qu'en mode billet seul (pas de timestamp dans les ids)
-// - notes dans l'extrait et notes dans le corps collisionnent (même ids)
-//
-// HTML :
-//
-// ABCDE = aléatoire (5 caractères alphabétiques en minuscule)
-// n = numéro de la note (1 à n) pour le billet
-//
-// Appel de note:
-// <sup data-footnote-id="ABCDE">
-//   <a href="#footnote-n" id="footnote-marker-n-1" rel="footnote">[n]</a>
-// </sup>
-// Note :
-// <li data-footnote-id="vzsmn" id="footnote-n">
-//   <sup><a href="#footnote-marker-n-1">^</a> </sup>...
-// </li>
-//
-// Il va falloir masquer le lien de retour dans la note affichée à la volée, CSS : a:[href qui débute par #footnote-marker-]
-//
-// À noter :
-// - notes dans l'extrait et notes dans le corps collisionnent (même ids)
-//
-// ----
-//
-// Notes d'utilisation : Uniquement en mode billet/page seule, pas de notes dans les extraits
-
 dotclear.ready(() => {
-  const scope = '.post';
+  const scope = '.post-content';
   const anchorPattern = /^#(fn|footnote|wiki-footnote)[:\-_\d]/gi;
   const returnPattern = /^#(fnref|footnote-marker|rev-wiki-footnote)[:\-_\d]/gi;
 
@@ -77,18 +11,85 @@ dotclear.ready(() => {
     (link) => link.href.startsWith(document.location.href) && link.hash.match(anchorPattern) && !link.hash.match(returnPattern),
   );
 
-  // Recherche des notes correspondantes :
-  const notes = [];
-  for (const link of links) {
-    const id = link.hash.slice(1); // ID de la note
-    const note = document.getElementById(id);
-    if (note) {
-      const item = note.tagName === 'A' ? note.parentNode() : note;
-      if (item.tagName === 'P' || item.tagName === 'LI') {
-        notes[id] = item;
+  if (links.length > 0) {
+    const footnotes = document.querySelector(`${scope} .footnotes`);
+    if (footnotes) {
+      // Clone footnote div with content
+      const clone = footnotes.cloneNode(true);
+      // Remove headers and hrs from clone
+      for (const child of clone.children) {
+        if (child.tagName === 'HR' || child.tagName.match(/H[1-6]/)) {
+          child.remove();
+        }
       }
+
+      // Add clone after standard footnotes div
+      clone.classList.add('notebox');
+      footnotes.classList.add('cloned');
+      footnotes.after(clone);
+
+      // Offset helper
+      const offset = function (el) {
+        const box = el.getBoundingClientRect();
+        return {
+          top: box.top + window.pageYOffset - document.documentElement.clientTop,
+          left: box.left + window.pageXOffset - document.documentElement.clientLeft,
+        };
+      };
+
+      // Notes visibility helper
+      // A better solution will be to use intersection observer for each link
+      const check = function (note) {
+        const noteoff = offset(note);
+        const notesoff = offset(document.querySelector(`${scope} .footnotes.cloned`));
+
+        const idnote = note.getAttribute('href').replace(/^.*#/, '');
+
+        let iidnote = clone.querySelector(`.notebox #${CSS.escape(idnote)}`);
+        if (iidnote.tagName === 'A') iidnote = iidnote.parentNode;
+
+        const niveau = window.innerHeight / 1.5;
+        const nivhaut = window.innerHeight / 10;
+
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+        if (
+          noteoff && // Valid coordinates
+          scrollTop > noteoff.top - niveau &&
+          scrollTop < noteoff.top - nivhaut &&
+          scrollTop < notesoff.top - niveau
+        ) {
+          iidnote.classList.add('show_note');
+          iidnote.classList.remove('hide_note');
+        } else {
+          iidnote.classList.add('hide_note');
+          iidnote.classList.remove('show_note');
+        }
+      };
+
+      // Display or hide cloned notes
+      const display = function () {
+        // Loop on footnotes in content
+        for (const link of links) {
+          check(link);
+        }
+        if (clone.querySelectorAll('.show_note').length > 0) clone.classList.add('present');
+        else clone.classList.remove('present');
+      };
+
+      // 1st display
+      display();
+
+      // Check on window scroll
+      window.addEventListener('scroll', display());
+
+      // Check on window resize
+      window.addEventListener('resize', display());
+
+      // Check every 5 seconds
+      setTimeout(() => {
+        // display();
+      }, 5000);
     }
   }
-
-  // Création de la div d'affichage à la volée avec les notes disponibles
 });
