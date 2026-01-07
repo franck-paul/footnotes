65ef16d26352605e6c4f774dec857931246ba4de
franck <carnet.franck.paul@gmail.com>
2026-01-07 16:35:26 +0100

    French locales

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,8 +10,9 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Footnotes'] = 'Notes de bas de page à la volée';
 L10n::$locales['footnotes'] = 'footnotes';
+L10n::$locales['Footnotes'] = 'Notes de bas de page à la volée';
 L10n::$locales['Enable Footnotes'] = 'Activer les notes de bas de page à la volée';
 L10n::$locales['Options'] = 'Options';
 L10n::$locales['Activate only in single entry context'] = 'Activer uniquement dans les contextes d\'entrée unique (billet ou page)';
+L10n::$locales['Set footnotes background'] = 'Définir l\'arrière-plan des notes de bas de page';
