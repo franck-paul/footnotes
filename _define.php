242a7ba40587dd8c262f8ce367b4720c61e1f3b8
franck <contact@open-time.net>
2026-08-13 16:41:14 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,23 +11,27 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'footnotes',
-    'Footnotes',
-    'Franck Paul',
-    '4.0',
-    [
-        'date'        => '2003-08-13T13:42:00+04.0',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => [
-            'blog' => '#params.footnotes',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=footnotes',
-        'support'    => 'https://github.com/franck-paul/footnotes',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/footnotes/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'footnotes',
+        'Footnotes',
+        'Franck Paul',
+        '4.0',
+        [
+            'date'        => '2003-08-13T13:42:00+04.0',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => [
+                'blog' => '#params.footnotes',
+            ],
+
+            'details'    => 'https://open-time.net/?q=footnotes',
+            'support'    => 'https://github.com/franck-paul/footnotes',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/footnotes/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
